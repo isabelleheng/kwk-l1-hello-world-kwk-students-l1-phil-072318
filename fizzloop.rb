a5be71725ebac32490495ec4004414f3f5259638
isabelleheng <isabellejhheng@gmail.com>
2018-07-24 19:49:47 +0000

    automatically backed up by learn

--- fizzloop.rb
+++ fizzloop.rb
@@ -1,15 +1,16 @@
 def counter
 number = 0
   while number < 11
+  number
   number +=1
 end
 
 def fizzbuzz
-  if number %15== 0 
+  if number %15 == 0 
     return "fizzbuzz"
-  elsif number %3== 0 
+  elsif number %3 == 0 
     return "fizz"
-  elsif number %5== 0 
+  elsif number %5 == 0 
     return "buzz"
   else 
     return "nil"
