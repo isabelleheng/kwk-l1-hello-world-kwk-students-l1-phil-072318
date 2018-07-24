cbf74aa57b3319e84e8d582e9cbdbe1d209a329e
isabelleheng <isabellejhheng@gmail.com>
2018-07-24 19:46:25 +0000

    automatically backed up by learn

--- fizzloop.rb
+++ fizzloop.rb
@@ -5,13 +5,10 @@ number = 0
 end
   if number %15 == 0 
     return "fizzbuzz"
-  end
   elsif number %3 == 0 
     return "fizz"
-  end
   elsif number %5 == 0 
     return "buzz"
-  end
   else 
     return "nil"
   end
